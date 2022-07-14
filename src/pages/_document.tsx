c9a9494786a1c599522b9b900c1a93757418d4e1
Micael Gomes <oi@micaelgomes.com>
2022-07-14 09:47:22 -0300

    M - Adição de Script de cookies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
+import Script from "next/script";
 
 export default class Document extends NextDocument {
   render() {
@@ -7,10 +8,10 @@ export default class Document extends NextDocument {
       <Html>
         <Head />
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
+          <Script src="https://tag.goadopt.io/injector.js%3Fwebsite_code=83251943-8ca0-4eab-8dbb-3618f34def94"></Script>
         </body>
       </Html>
     );
